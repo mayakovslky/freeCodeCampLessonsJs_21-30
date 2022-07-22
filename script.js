c7eef5255fd58a40a2f44f3d9b513ff250c4dfb1
Vladimir Drozdov <mayakovsky03@gmail.com>
2022-07-22 15:11:35 +0300

    lessonsJs 21-30

---
+++ script.js
@@ -0,0 +1,80 @@
+// 21. Compound Assignment With Augmented Addition
+
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// a += 12;
+// b += 9;
+// c += 7;
+
+// 22. Compound Assignment With Augmented Subtraction
+
+// let a = 11;
+// let b = 9;
+// let c = 3;
+
+// a -= 6;
+// b -= 15;
+// c -= 1;
+
+// 23. Compound Assignment With Augmented Multiplication
+/*
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+*/
+
+// 24. Compound Assignment With Augmented Division
+/*
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+*/
+
+// 25. Escaping Literal Quotes in Strings
+/*
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+*/
+
+// 26. Quoting Strings with Single Quotes
+
+/* 
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+*/
+
+// 27. Escape Sequences in Strings
+
+/*
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+*/
+
+// 28. Concatenating Strings with Plus Operator
+
+/*
+const myStr = "This is the start. " + "This is the end.";
+*/
+
+// 29. Concatenating Strings with the Plus Equals Operator
+
+/*
+let myStr = "This is the first sentence.";
+
+myStr +=" This is the second sentence.";
+*/
+
+// 30. Constructing Strings with Variables
+
+/*
+const myName = "Vladimir";
+const myStr = "My name is " + myName + "and I am well";
+*/
